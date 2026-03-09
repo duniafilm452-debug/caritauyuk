957e8708aa15444f6a3b0bfef93ad4910422576b
Shihao <duniafilm452@gmail.com>
2026-03-09 21:08:27 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // config.js — Konfigurasi Supabase & Konstanta Global
 // ============================================================
 
-const supabaseUrl      = 'https://api.yukcaritau.my.id';
+const supabaseUrl      = 'https://ucguioolhvytinvqnirk.supabase.co';
 const supabaseAnonKey  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjZ3Vpb29saHZ5dGludnFuaXJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NjM0MTksImV4cCI6MjA4NzMzOTQxOX0.Y9ETtIB6QMj5D6Wp278TOLGWKFKf04ecpLHzQ3Wq2cg';
 
 // ── Inisialisasi Supabase client ──────────────────────────────
